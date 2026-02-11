17f016bda1fb4235f71512a125235c3ce5fdb742
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2026-02-12 00:31:04 +0600

    20.7 validation

--- product/serializers.py
+++ product/serializers.py
@@ -52,3 +52,8 @@ class ProductSerializer(serializers.ModelSerializer):
 
     def calculate_tax(self, product):
         return round(product.price * Decimal(1.1), 2)
+    
+    def validate_price(self,price):
+        if price<0:
+            raise serializers.ValidationError("Please could not be negative")
+        return price
