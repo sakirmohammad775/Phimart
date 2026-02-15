809503a104d1353c05bef703d988fa98265de9cd
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2026-02-16 02:12:35 +0600

    add 22.6,7 addded update delete put

--- order/models.py
+++ order/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from users.models import User
 from product.models import Product
 from uuid import uuid4
+from django.core.validators import MinValueValidator
 # Create your models here.
 
 
@@ -19,7 +20,7 @@ class CartItem(models.Model):
     cart = models.ForeignKey(
         Cart, on_delete=models.CASCADE, related_name="items")
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField(validators=[MinValueValidator(1)])
 
     class Meta:
         unique_together=[['cart','product']]
--- order/serializers.py
+++ order/serializers.py
@@ -4,14 +4,39 @@ from product.models import Product
 from product.serializers import ProductSerializer
 
 
-class simpleProductSerializer(serializers.ModelSerializer):
+class SimpleProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ["id", "name", "price"]
 
 
+class AddCartItemSerializer(serializers.ModelSerializer):
+    product_id = serializers.IntegerField()
+
+    class Meta:
+        model = CartItem
+        fields = ["id", "product_id", "quantity"]
+
+    def save(self, **kwargs):
+        cart_id = self.context["cart_id"]
+        product_id = self.validated_data["product_id"]
+        quantity = self.validated_data["quantity"]
+
+        try:
+            cart_item = CartItem.objects.get(cart_id=cart_id, product_id=product_id)
+            cart_item.quantity += quantity
+            self.instance = cart_item.save()
+        except CartItem.DoesNotExist:
+            CartItem.objects.create(cart_id=cart_id, **self.validated_data)
+        return self.instance
+    
+    def validate_product_id(self,value):
+        if not Product.objects.filter(pk=value).exists():
+            raise serializers.ValidationError(f"Product with {value} does not exists")
+        return value
+    
 class CartItemSerializer(serializers.ModelSerializer):
-    product = simpleProductSerializer()
+    product = SimpleProductSerializer()
     total_price = serializers.SerializerMethodField(method_name="get_total_price")
 
     class Meta:
@@ -23,7 +48,7 @@ class CartItemSerializer(serializers.ModelSerializer):
 
 
 class CartSerializer(serializers.ModelSerializer):
-    items = CartItemSerializer(many=True,read_only=True)
+    items = CartItemSerializer(many=True, read_only=True)
     total_price = serializers.SerializerMethodField(method_name="get_total_price")
 
     class Meta:
@@ -32,3 +57,8 @@ class CartSerializer(serializers.ModelSerializer):
 
     def get_total_price(self, cart: Cart):
         return sum([item.product.price * item.quantity for item in cart.items.all()])
+
+class UpdateCartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=CartItem
+        fields=['quantity']
