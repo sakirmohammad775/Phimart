b8708d5cb3c48ffe2d511143d3c64cfc7f4ca875
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2026-02-15 08:57:26 +0600

    add 21.7 Filtering & Generic Filter

--- api/urls.py
+++ api/urls.py
@@ -3,10 +3,10 @@ from product.views import ProductViewSet, CategoryViewSet, ReviewViewSet
 from rest_framework_nested import routers
 
 router = routers.DefaultRouter()
-router.register("products", ProductViewSet)
+router.register("products", ProductViewSet,basename='products')
 router.register("categories", CategoryViewSet)
 
-product_router = routers.NestedDefaultRouter(router, "products", lookup="product")
+product_router = routers.NestedDefaultRouter(router, "products",lookup="product")
 product_router.register("reviews", ReviewViewSet, basename="product-review")
 
 # urlpatterns = router.urls
--- product/views.py
+++ product/views.py
@@ -4,11 +4,21 @@ from product.models import Product, Category,Review
 from product.serializers import ProductSerializer, CategorySerializer,ReviewSerializer
 from django.db.models import Count
 from rest_framework.viewsets import ModelViewSet
-
+from django_filters.rest_framework import DjangoFilterBackend
+from product.filters import ProductFilter
 
 class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.all()
+    queryset=Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends=[DjangoFilterBackend]
+    filterset_class=ProductFilter
+    
+    # def get_queryset(self):
+    #     queryset = Product.objects.all()
+    #     category_id=self.request.query_params.get('category_id')
+    #     if category_id is not None:
+    #         queryset=Product.objects.filter(category_id=category_id)
+    #     return queryset
 
     def destroy(self, request, *args, **kwargs):
         product = self.get_object()
