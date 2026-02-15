bc9643fa8fee2b5cd51ac2cdd44d1022b8ddcf84
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2026-02-15 14:12:56 +0600

    22.2 Creating & Getting Cart

--- order/views.py
+++ order/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import CreateModelMixin,RetrieveModelMixin
+from order.models import Cart
+from order.serializers import CartSerializer
+
+class CartViewSet(CreateModelMixin,GenericViewSet,RetrieveModelMixin):
+    queryset=Cart.objects.all()
+    serializer_class=CartSerializer
+    
+
+
+
+
+
+
+
+
+
+
 
 # Create your views here.
+#Step to build an ApI
+#model
+#serializer
+#Viewset
+#router
