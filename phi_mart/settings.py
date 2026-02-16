a2952d724897b8fb28bae8460d9e2da659cf0035
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2026-02-16 19:55:22 +0600

    add 23.3,4 Resgistering User ApI and Login User using JWT

--- phi_mart/settings.py
+++ phi_mart/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -144,4 +144,11 @@ REST_FRAMEWORK = {
 }
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
+   'ACCESS_TOKEN_LIFETIME':timedelta(days=5),
+}
+
+DJOSER={
+    'SERIALIZERS':{
+        'user_create':'users.serializers.UserCreateSerializer'
+    },
 }
