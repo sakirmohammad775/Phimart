2285096f3f9ee79602191e96d5f41fbb6c73f182
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2026-03-01 21:54:29 +0600

    add def create in cartVeeiewset upload for create or get cart

--- order/views.py
+++ order/views.py
@@ -19,8 +19,10 @@ from order.serializers import (
     CreateOrderSerializer,
     UpdateOrderSerializer,
 )
-from rest_framework.permissions import IsAuthenticated,IsAdminUser
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from order.services import OrderService
+from rest_framework import status
+
 
 class CartViewSet(
     CreateModelMixin, DestroyModelMixin, GenericViewSet, RetrieveModelMixin
@@ -32,12 +34,20 @@ class CartViewSet(
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-    
     def get_queryset(self):
-        if getattr(self, 'swagger_fake_view', False):
+        if getattr(self, "swagger_fake_view", False):
             return Cart.objects.none()
-        return Cart.objects.prefetch_related('items__product').filter(user=self.request.user)
+        return Cart.objects.prefetch_related("items__product").filter(
+            user=self.request.user
+        )
 
+    def create(self, request, *args, **kwargs):
+        existing_cart = Cart.objects.filter(user=request.user).first()
+
+        if existing_cart:
+            serializer = self.get_serializer(existing_cart)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return super().create(request, *args, **kwargs)
 
 
 class CartItemViewSet(ModelViewSet):
@@ -52,10 +62,10 @@ class CartItemViewSet(ModelViewSet):
 
     def get_serializer_context(self):
         context = super().get_serializer_context()
-        if getattr(self, 'swagger_fake_view', False):
+        if getattr(self, "swagger_fake_view", False):
             return context
-        
-        return {'cart_id': self.kwargs.get('cart_pk')}
+
+        return {"cart_id": self.kwargs.get("cart_pk")}
 
     def get_queryset(self):
         return CartItem.objects.select_related("product").filter(
@@ -65,30 +75,31 @@ class CartItemViewSet(ModelViewSet):
 
 class OrderViewSet(ModelViewSet):
 
-    http_method_names=['get','post','delete','patch','head','options']
+    http_method_names = ["get", "post", "delete", "patch", "head", "options"]
 
-    @action(detail=True, methods=['post'])
+    @action(detail=True, methods=["post"])
     def cancel(self, request, pk=None):
         order = self.get_object()
         OrderService.cancel_order(order=order, user=request.user)
-        return Response({'status': 'Order canceled'})
+        return Response({"status": "Order canceled"})
 
-    @action(detail=True, methods=['patch'])
+    @action(detail=True, methods=["patch"])
     def update_status(self, request, pk=None):
         order = self.get_object()
         serializer = orderSz.UpdateOrderSerializer(
-            order, data=request.data, partial=True)
+            order, data=request.data, partial=True
+        )
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response({'status': f'Order status updated to {request.data['status']}'})
-    
+        return Response({"status": f"Order status updated to {request.data['status']}"})
+
     def get_permissions(self):
-        if self.action in ['update_status','destroy']:
+        if self.action in ["update_status", "destroy"]:
             return [IsAdminUser()]
         return [IsAuthenticated()]
-    
+
     def get_serializer_class(self):
-        if self.action=='cancel':
+        if self.action == "cancel":
             return orderSz.EmptySerializer
         if self.action == "create":
             return orderSz.CreateOrderSerializer
@@ -97,12 +108,12 @@ class OrderViewSet(ModelViewSet):
         return orderSz.OrderSerializer
 
     def get_serializer_context(self):
-        if getattr(self, 'swagger_fake_view', False):
+        if getattr(self, "swagger_fake_view", False):
             return super().get_serializer_context()
-        return {"user_id": self.request.user.id,'user':self.request.user}
+        return {"user_id": self.request.user.id, "user": self.request.user}
 
     def get_queryset(self):
-        if getattr(self, 'swagger_fake_view', False):
+        if getattr(self, "swagger_fake_view", False):
             return Order.objects.none()
         if self.request.user.is_staff:
             return Order.objects.prefetch_related("items__product").all()
